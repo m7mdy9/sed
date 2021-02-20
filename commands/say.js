17cbcbae791d44259dd5af2554e19004c87e2a87
m7mdy9 <hamestar1122@gmail.com>
2021-02-20 14:33:25 +0200

    F

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,7 @@ module.exports ={
     expectedArgs: "wrong usage \nusage: {PREFIX}<channelID or channel mention> <msg>",
     callback: async ({ message, client, args, prefix }) =>{
         
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You do not have premissions to do that!');
         const channel = client.channels.cache.get(args[0]) || message.mentions.channels.first()
         const mm = args.slice(1).join(" ")
         try {  
