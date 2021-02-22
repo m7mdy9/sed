3ed4aa341896bf1dc8dfcdbe4339029d95ccabaa
m7mdy9 <hamestar1122@gmail.com>
2021-02-22 16:11:16 +0200

    lol

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ module.exports ={
     aliases: ["s"],
     ownerOnly: true,
     minArgs: 1,
-    expectedArgs: "wrong usage how to use:{PREFIX}<channelID/Channel_mention> <msg>",
+    expectedArgs: "{PREFIX}<channelID/Channel_mention> <msg>",
     callback: async ({ message, client, args, prefix }) =>{
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You do not have premissions to do that!');
         const channel = client.channels.cache.get(args[0]) || message.mentions.channels.first()
