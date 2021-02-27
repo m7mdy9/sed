0c49357dbdd2e69735a5096790312af834f1b1b0
m7mdy9 <hamestar1122@gmail.com>
2021-02-27 21:10:52 +0200

    mmm

---
+++ commands/eval.js
@@ -0,0 +1,48 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    ownerOnly: true,
+    name: 'eval',
+    aliases: ["e"],
+    minArgs: 1,
+    maxArgs: -1,
+    syntaxError: 'Incorrect syntax! Use `{PREFIX}eval <code or what ever>`',
+    description: 'Evalutions a code',
+    category: 'Owner',
+    callback: async ({client, message, args}) => {
+        const embed = new MessageEmbed()
+            .setTitle('<a:loading:779681208954126357> Evaluating...')
+            .setColor('#000000')
+        const msg = await message.channel.send(embed);
+        try {
+            const data = eval(args.join(' ').replace(/```/g, ''));
+            const embed = new MessageEmbed()
+                .setTitle('Output: ')
+                .setColor('#000000')
+                .setDescription(await data)
+            await msg.edit(embed)
+            await msg.react('✔️')
+            await msg.react('❌')
+            const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✔️') && (user.id === message.author.id);
+            msg.awaitReactions(filter, { max: 1 })
+                .then((collected) => {
+                    collected.map((emoji) => {
+                        switch (emoji._emoji.name) {
+                            case '✔️':
+                                msg.reactions.removeAll();
+                                break;
+                            case '❌':
+                                msg.delete()
+                                break;
+                        }
+                    })
+                })
+        } catch (e) {
+            const embed = new MessageEmbed()
+                .setTitle('An Error has occured')
+                .setColor('#000000')
+            return await msg.edit(embed);
+
+        }
+    }
+}
