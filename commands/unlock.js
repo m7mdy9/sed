d73f29190fd0e7adb6807f95310377885711f26e
m7mdy9 <hamestar1122@gmail.com>
2021-02-28 23:24:56 +0200

    umm

--- commands/unlock.js
+++ commands/unlock.js
@@ -1,7 +1,7 @@
 module.exports ={
     aliases: 'ul',
     callback: ({ message, args }) =>{
-        if (!message.member.hasPermission("MANAGE_CHANNELS") && !message.guild.ownerID === message.author.id) {
+        if (!message.member.hasPermission("MANAGE_CHANNELS")) {
             message.channel.send('You don\'t have Permission to do that')
         }
         const role = message.guild.roles.cache.get(`${message.guild.id}`)
