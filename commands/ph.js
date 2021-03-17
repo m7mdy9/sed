92f9ebdf8d7f00248fe3ea819c59acb997d7732d
m7mdy9 <hamestar1122@gmail.com>
2021-03-17 18:28:13 +0200

    mhm

---
+++ commands/ph.js
@@ -0,0 +1,7 @@
+module.exports ={
+    callback: ({ message, args }) =>{
+        if (message.channel.id != "797748808679161856") return;
+       
+       message.channel.send(`<@&798120683086938112>: ${args}`)
+    }
+}
