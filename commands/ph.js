2fb6a34b3917db9e3d577d31a4a8fd7abd8cc0e8
m7mdy9 <hamestar1122@gmail.com>
2021-03-17 18:52:40 +0200

    fix

--- commands/ph.js
+++ commands/ph.js
@@ -1,5 +1,6 @@
 module.exports ={
     callback: ({ message, args }) =>{
+        message.delete()
         if (message.channel.id != "797748808679161856") return;
        
        message.channel.send(`<@&798120683086938112>: ${args}`)
