6c9b737713675eb23af4ac25477c721ef04bfbe6
m7mdy9 <hamestar1122@gmail.com>
2021-03-21 12:50:03 +0200

    fix ig

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js') 
+const Discord = require('discord.js')
 require('dotenv').config()
 const WOKcommands = require("wokcommands");
 const client = new Discord.Client({
