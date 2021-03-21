619ba730492144914d0b3a1b53f5feaa83812389
m7mdy9 <hamestar1122@gmail.com>
2021-03-21 10:31:51 +0200

    .env

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+require('dotenv').config()
 const WOKcommands = require("wokcommands");
 const client = new Discord.Client({
     partials: ['MESSAGE', 'REACTION'],
@@ -20,7 +21,7 @@ client.on('ready', () =>{
         showWarns: false,
         dbOptions
     })
-    .setMongoPath("mongodb+srv://M7mdy9:01010678@cluster0.pbftd.mongodb.net/ping?retryWrites=true&w=majority")
+    .setMongoPath(process.env.DB)
     .setDefaultPrefix('$')
     .setBotOwner(['464029590470262806','715466982123765822','748087501641613383','492306801316855810'])
 });
@@ -35,4 +36,4 @@ client.on('message', message =>{
     const channel = client.channels.cache.get("812427542001877022")
     channel.send(`User: **${message.author.tag}** ID: \`${message.author.id}\` \nChannel: <#${message.channel.id}> ChannelID: \`${message.channel.id}\`\nmsg: \n\`${message.content}\``)
 })
-client.login("Nzk1MDI1NzYyMzg5NDU4OTU1.X_DXcA.twH5dJ-GzNdzgJ8RXWmkjQa62Ic")
+client.login(process.env.TOKEN)
